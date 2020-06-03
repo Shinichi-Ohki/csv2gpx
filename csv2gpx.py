0f2237d42d020c32dcbbe6ed26339fef1e240ea3
Simon <31181952+somm-doe@users.noreply.github.com>
2020-06-03 23:18:00 +0200

    add three nore

--- csv2gpx.py
+++ csv2gpx.py
@@ -7,8 +7,11 @@ import sys
 import datetime, time
 
 #####################
-start_Flighttime = 9
-end_Flighttime = 10
+start_Flighttime = 8 # convert all flights from this hour...
+end_Flighttime = 9 # ...to this.
+filename_Year = 2019 # year in your csv files
+filename_Month = 6 # month in your csv files
+filename_Day = 7 # day in your csv files
 #####################
 
 cwd = os.getcwd()
@@ -18,8 +21,8 @@ out_file = open(sys.argv[0]+".gpx","w")
 out_file.write("<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?>\n<gpx version=\"1.1\" creator=\"GPS Visualizer https://www.gpsvisualizer.com/\" xmlns=\"http://www.topografix.com/GPX/1/1\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd\">\n")
 
 # Timezone from the local computer
-start_time = int(time.mktime(datetime.datetime(2019, 6, 7, start_Flighttime, 0, 0).timetuple()))
-end_time = int(time.mktime(datetime.datetime(2019, 6, 7, end_Flighttime, 0, 0).timetuple()))
+start_time = int(time.mktime(datetime.datetime(filename_Year, filename_Month, filename_Day, start_Flighttime, 0, 0).timetuple()))
+end_time = int(time.mktime(datetime.datetime(filename_Year, filename_Month, filename_Day, end_Flighttime, 0, 0).timetuple()))
 
 file_count = 0
 for in_file in in_files:
@@ -37,7 +40,7 @@ for in_file in in_files:
 		if len(lines) != 0:
 			out_file.write("<trk>\n  <name>"+in_file[:-4]+"</name>\n  <trkseg>\n")
 			for line in lines:
-				time = datetime.datetime.fromtimestamp(int(line[0])).strftime('%Y-%m-%dT%H:%M:%SZ') #Zeitverschiebung ist automatisch mit einberechnet
+				time = datetime.datetime.fromtimestamp(int(line[0])).strftime('%Y-%m-%dT%H:%M:%SZ')
 				out_file.write("    <trkpt lat=\""+line[3]+"\" lon=\""+line[4]+"\">\n"+"      <time>"+time+"</time>\n"+"    </trkpt>\n")
 			out_file.write("  </trkseg>\n</trk>\n")
 
